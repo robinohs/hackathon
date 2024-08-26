83bfee138aaf3cf3764ab93dce78bcdb6757cbce
Robin Ohs <ohs@depend.uni-saarland.de>
2024-08-26 20:51:01 +0200

    use cp instead of mv

--- run.sh
+++ run.sh
@@ -1,11 +1,9 @@
-sudo apt-get install pipx
+apt-get install pipx
 pipx install poetry
-sudo mv ./carbond /usr/bin/carbond
+sudo cp ./carbond /usr/bin/carbond
 sudo mkdir /var/carbond
 sudo mkdir /etc/carbond
 sudo chown peach:peach /var/carbond
 sudo chown peach:peach /etc/carbond
 mkdir ~/carbond-hackathon
-mv ./measurements ~/carbond-hackathon
-cd ~/carbond-hackathon/measurements
-poetry install
+cp -r ./measurements ~/carbond-hackathon
